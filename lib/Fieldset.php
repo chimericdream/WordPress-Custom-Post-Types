9e0bd6259a2651ef27f3867a1a8170b1800a1a1e
Bill Parrott <bill@chimericdream.com>
2014-04-25 22:02:22 -0500

    adding fieldset class

---
+++ lib/Fieldset.php
@@ -0,0 +1,19 @@
+<?php
+class WPCPT_Fieldset {
+    protected $fields = array();
+
+    public function addField(WPCPT_Field $field) {
+        $this->fields[] = $field;
+    }
+
+    public function render() {
+        if (empty($this->fields)) {
+            return;
+        }
+        echo '<table class="form-table">';
+        foreach ($this->fields as $f) {
+            $f->render();
+        }
+        echo '</table>';
+    }
+}
