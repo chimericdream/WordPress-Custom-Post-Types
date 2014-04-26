7e09b6c32a9949f07a421450a05291855f058fcc
Bill Parrott <bill@chimericdream.com>
2014-04-25 22:30:58 -0500

    adding nowysiwyg field

---
+++ lib/Field/Textarea/NoWysiwyg.php
@@ -0,0 +1,18 @@
+<?php
+class WPCPT_Field_Textarea_NoWysiwyg extends WPCPT_Field_Textarea {
+    public function renderField() {
+        echo '<div class="plain-textarea-wrap">';
+        if (!empty($this->note)) {
+            echo "<p><strong>Note:</strong> {$this->note}</p>";
+        }
+        echo "<textarea id=\"{$this->fieldId}\" name=\"";
+        if (empty($this->fieldName)) {
+            echo $this->fieldId;
+        } else {
+            echo $this->fieldName;
+        }
+        echo "\"";
+        $this->renderAttributes();
+        echo ">{$this->value}</textarea></div>";
+    }
+}
