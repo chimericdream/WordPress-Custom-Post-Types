fffd3a5534a5f5fbd7ce07eb4a2e541c2a2b3076
Bill Parrott <bill@chimericdream.com>
2014-04-25 22:15:48 -0500

    adding text field

---
+++ lib/Field/Text.php
@@ -0,0 +1,13 @@
+<?php
+class WPCPT_Field_Text extends WPCPT_Field {
+    public function renderField() {
+        echo "<input type=\"text\" id=\"{$this->fieldId}\" name=\"";
+        echo (!empty($this->fieldName)) ? $this->fieldName : $this->fieldId;
+        echo '"';
+        $this->renderAttributes();
+        echo " value=\"{$this->value}\">";
+        if (!empty($this->note)) {
+            echo '<p class="description">' . $this->note . '</p>';
+        }
+    }
+}
